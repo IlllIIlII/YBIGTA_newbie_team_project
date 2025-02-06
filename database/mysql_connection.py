3ab1d9874cafee92bdf18e402aa2972b8add49c4
seohyun <ttony0709@naver.com>
2025-02-06 19:15:31 +0900

    update mysql

--- database/mysql_connection.py
+++ database/mysql_connection.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,7 +7,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+user = os.getenv("DB_USER")
+passwd = os.getenv("DB_PASSWD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db = os.getenv("DB_NAME")
+
 DB_URL = f'mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}?charset=utf8'
 
 engine = create_engine(DB_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# 모델 정의
+class User(Base):
+    __tablename__ = 'users'
+    
+    email = Column(String(100), primary_key=True, unique=True, index=True)
+    username = Column(String(50), nullable=False)
+    password = Column(String(255), nullable=False)
+
+# 테이블 생성
+Base.metadata.create_all(bind=engine)
